cc2afaf3bb369592a0758584de6c1cd422e31a15
cxynueuwu <q2077027542@gmail.com>
2023-06-29 06:25:15 +0200

    Friend Update

--- script/clickEvents.js
+++ script/clickEvents.js
@@ -22,10 +22,53 @@ function incrementClickCount() {
     }, 500);
 }
 
+function showOption() {
+    var optionmenu = document.getElementById("options");
+    if (optionmenu.classList.contains("show")) {
+        optionmenu.classList.replace("show", "hide");
+    } else if (optionmenu.classList.contains("hide")) {
+        optionmenu.classList.replace("hide", "show");
+    }
+}
+
+function showFriend() {
+    var friendmenu = document.getElementById("friends");
+    if (friendmenu.classList.contains("show")) {
+        friendmenu.classList.replace("show", "hide");
+    } else if (friendmenu.classList.contains("hide")) {
+        friendmenu.classList.replace("hide", "show");
+    }
+}
+
+function inviteFriend(potatoes) {
+    var clickCountElement = document.getElementById("click-count");
+    var currentValue = parseInt(clickCountElement.textContent);
+    var leftValue = currentValue - potatoes;
+    if (leftValue >= 0) {
+        clickCountElement.textContent = leftValue;
+        var content = "Thanks for the potatoes!"
+        var audio = new Audio('./sound/upgrade-sound.mp3');
+        audio.play();
+    } else {
+        var content = "Sorry, but you don't have enough potato...";
+        var audio = new Audio('./sound/error-sound.mp3');
+        audio.play();
+    }
+    const notification = document.createElement('div');
+    notification.textContent = content;
+    notification.classList.add('notification');
+    document.body.appendChild(notification);
+
+    setTimeout(() => {
+        document.body.removeChild(notification);
+    }, 1000);
+
+}
+
 function noShack() {
     var potatoImage = document.getElementById("potato-image");
-    var checkbox = document.getElementById("no-shack");
-    if (checkbox.checked) {
+    var checkbox = document.getElementById("shack");
+    if (!checkbox.checked) {
         potatoImage.style.animation = "none";
     } else {
         potatoImage.style.animation = "";
